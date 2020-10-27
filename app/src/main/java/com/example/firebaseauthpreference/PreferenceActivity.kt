56c79fd93bf264857f29a362a7de60c00eb8d345
Kwounsu <sb00lgs@gmail.com>
2020-10-27 11:52:54 -0700

    preference secure register

--- app/src/main/java/com/example/firebaseauthpreference/PreferenceActivity.kt
+++ app/src/main/java/com/example/firebaseauthpreference/PreferenceActivity.kt
@@ -2,10 +2,14 @@ package com.example.firebaseauthpreference
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import kotlinx.android.synthetic.main.activity_preference.*
 import kotlinx.android.synthetic.main.activity_preference.btn_login
 import kotlinx.android.synthetic.main.activity_preference.btn_logout
@@ -15,21 +19,42 @@ import kotlinx.android.synthetic.main.activity_preference.et_password
 import kotlinx.android.synthetic.main.activity_preference.tv_message
 
 class PreferenceActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preference)
 
-        val sharedPreference =  getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
-        val editor = sharedPreference.edit()
+//        val sharedPreference =  getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+//        val editor = sharedPreference.edit()
+
+        val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+        val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            "shared_preferences_filename",
+            masterKeyAlias,
+            this,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+        // reading a value
+        sharedPreferences.getString("some_key", "some_default_value") // -> "some_data"
 
         btn_register.setOnClickListener {
-            editor.putString(et_email.text.toString(),et_password.text.toString())
-            editor.apply()
+            // storing a value Securely
+            sharedPreferences
+                .edit()
+                .putString(et_email.text.toString(), et_password.text.toString())
+                .apply()
+
+//            editor.putString(et_email.text.toString(),et_password.text.toString())
+//            editor.apply()
             Toast.makeText(this, "Register Success", Toast.LENGTH_LONG).show()
         }
 
         btn_login.setOnClickListener {
-            val pwd = sharedPreference.getString(et_email.text.toString(),"null")
+            // reading a value
+            val pwd = sharedPreferences.getString(et_email.text.toString(), "null")
+//            val pwd = sharedPreference.getString(et_email.text.toString(),"null")
             if (et_password.text.toString() == pwd) {
                 signInSuccess()
             }else {
